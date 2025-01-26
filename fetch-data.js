92f84ec3282d0d8ceb24ac5522bff6b9cbd2fa44
Tentswalo <tintswalototsndlovu@gmail.com>
2025-01-26 08:51:49 +0200

    Second commit

---
+++ fetch-data.js
@@ -0,0 +1,21 @@
+async function fetchUserData () {
+
+    const apiURL = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById("api-data");
+
+    try {
+        const response = await fetch(apiURL);
+        const users = await response.json();
+        dataContainer.innerHTML = '';
+        const userList = document.createElement('<ul>');
+        users.forEach((user) => {
+            const listItem = document.createElement('<ul>');
+            listItem.textContent = user.name;
+            userList.appendChild(listItem);
+        });
+    } catch (error) {
+        dataContainer.innerHTML = 'Failed to load user data.';
+    }
+
+    document.addEventListener('DOMContentLoaded', fetchUserData);
+};
