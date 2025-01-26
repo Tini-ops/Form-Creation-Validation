abd631c7a39e925a5e4d67c85ef0c6dac3920b75
Tentswalo <tintswalototsndlovu@gmail.com>
2025-01-26 08:54:36 +0200

    fix url

--- fetch-data.js
+++ fetch-data.js
@@ -1,10 +1,10 @@
 async function fetchUserData () {
 
-    const apiURL = 'https://jsonplaceholder.typicode.com/users';
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     const dataContainer = document.getElementById("api-data");
 
     try {
-        const response = await fetch(apiURL);
+        const response = await fetch(apiUrl);
         const users = await response.json();
         dataContainer.innerHTML = '';
         const userList = document.createElement('<ul>');
